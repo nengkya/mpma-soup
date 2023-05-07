4bb60be4a82ab633a0f537293ee1a42482c4b5f0
nengkya <nengkya@yahoo.com>
2023-05-07 22:21:04 +0700

    Succed get all header into csv.

--- mpma.py
+++ mpma.py
@@ -16,9 +16,8 @@ class Pause:
 		pass
 
 	def pause(self, timer):
-		self.timer  = timer
 		action = ActionChains(self.driver)
-		action.pause(self.timer)
+		action.pause(timer)
 		action.perform()
 
 
@@ -26,58 +25,54 @@ class Se:
 	def __init__(self):
 		pass
 
-	def company(self, driver, list1, list2):
-		self.driver = driver
-		self.list1  = list1
-		self.list2  = list2
 
-		for tr in range(self.list1, self.list2 + 1):
-			Pause(self.driver).pause(1)
+	def get_header(self, driver):
+		fields = []
+		for list in range(1,19):
+			path = '/html/body/div[2]/div[1]/section[4]/div[1]/div[1]/div[2]/div[1]/div[2]/div[6]/ul[1]/li[' + str(list) + ']/div[1]/span[1]'
+			print(path)
+			header = driver.find_element(By.XPATH, path)
+			fields.append(header.text)
 
-			p = '/html/body/div[2]/div[1]/section[5]/div[1]/div[1]/div[2]/div[1]/div[2]/table[1]/tbody[1]/tr[' + str(tr) + ']/td[1]/span[1]/a'
-			wait = WebDriverWait(self.driver, 0)
-			clickable = EC.element_to_be_clickable((By.XPATH, p))
+		print(fields)
+		return fields 
+
+
+	def get_company(self, driver, list1, list2):
+		for tr in range(list1, list2 + 1):
+			Pause(driver).pause(1)
+
+			path = '/html/body/div[2]/div[1]/section[5]/div[1]/div[1]/div[2]/div[1]/div[2]/table[1]/tbody[1]/tr[' + str(tr) + ']/td[1]/span[1]/a'
+			wait = WebDriverWait(driver, 0)
+			clickable = EC.element_to_be_clickable((By.XPATH, path))
 			link = wait.until(clickable)
 			link.click()
-			li = driver.find_elements(By.CLASS_NAME, "flexi")
 
-			for element in li:
-
-				c  = '//*[@id="flexicontent"]/div[6]/ul/li[1]/div/span'
-				header1 = driver.find_element(By.XPATH, c)
-				print(header1.text)
-
-				d  = '//*[@id="flexicontent"]/div[6]/ul/li[2]/div/span'
-				header2 = driver.find_element(By.XPATH, d)
-				print(header2.text)
-
-				e = '//*[@id="flexicontent"]/div[6]/ul/li[3]/div/span'
-				header3 = driver.find_element(By.XPATH, e)
-				print(header3.text)
-
-				#field names 
-				fields = [header1.text, header2.text, header3.text] 
+			#name of csv file 
+			filename = "company.csv"
+			fields   = self.get_header(driver)
+				
+			#writing to csv file 
+			with open(filename, 'w') as csvfile: 
+				#creating a csv writer object 
+				csvwriter = csv.writer(csvfile) 
 					
-				#data rows of csv file 
-				rows = [
-					['Nikhil', 'COE', '2', '9.0'], 
-					['Sanchit', 'COE', '2', '9.1'], 
-					['Aditya', 'IT', '2', '9.3'], 
-					['Sagar', 'SE', '1', '9.5'], 
-					['Prateek', 'MCE', '3', '7.8'], 
-					['Sahil', 'EP', '2', '9.1']
-				]
-					
-				#name of csv file 
-				filename = "university_records.csv"
-					
-				#writing to csv file 
-				with open(filename, 'w') as csvfile: 
-					#creating a csv writer object 
-					csvwriter = csv.writer(csvfile) 
-						
-					#writing the fields 
-					csvwriter.writerow(fields) 
+				#writing the fields 
+				csvwriter.writerow(fields) 
+	
+				li = driver.find_elements(By.CLASS_NAME, "flexi")
+
+				for element in li:
+
+					#data rows of csv file 
+					rows = [
+						['Nikhil', 'COE', '2', '9.0'], 
+						['Sanchit', 'COE', '2', '9.1'], 
+						['Aditya', 'IT', '2', '9.3'], 
+						['Sagar', 'SE', '1', '9.5'], 
+						['Prateek', 'MCE', '3', '7.8'], 
+						['Sahil', 'EP', '2', '9.1']
+					]
 						
 					#writing the data rows 
 					csvwriter.writerows(rows)
@@ -92,9 +87,9 @@ class Se:
 		driver.get('https://www.mpmadirectory.org.my/all-members')
 
 		driver.execute_script("window.scrollTo(0,  650)")
-		self.company(driver,  1, 1)
+		self.get_company(driver,  1, 1)
 		driver.execute_script("window.scrollTo(0, 1200)")
-		#self.company(driver, 11, 20)
+		#self.get_company(driver, 11, 20)
 
 		driver.quit()
 
